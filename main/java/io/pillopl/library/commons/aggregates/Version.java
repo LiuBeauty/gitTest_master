5376b07de052f0332bf36ff5534630354c427c9b
jxliu <liujiang@126.com>
2024-09-25 23:08:51 +0800

    for test

--- main/java/io/pillopl/library/commons/aggregates/Version.java
+++ main/java/io/pillopl/library/commons/aggregates/Version.java
@@ -5,7 +5,9 @@ import lombok.Value;
 @Value
 public class Version {
     int version;
-
+    public void ForTest(){
+        System.out.println("123");
+    }
     public static Version zero() {
         return new Version(0);
     }

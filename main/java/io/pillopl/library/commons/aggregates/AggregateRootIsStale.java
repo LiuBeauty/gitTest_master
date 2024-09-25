1aa8df7139e0914fcc8a9740701b635a5839f0c3
jxliu <liujiang@126.com>
2024-09-25 23:02:05 +0800

    for test

--- main/java/io/pillopl/library/commons/aggregates/AggregateRootIsStale.java
+++ main/java/io/pillopl/library/commons/aggregates/AggregateRootIsStale.java
@@ -5,4 +5,7 @@ public class AggregateRootIsStale extends RuntimeException {
     public AggregateRootIsStale(String msg) {
         super(msg);
     }
+    public void ForTest(){
+        System.out.println("123");
+    }
 }
